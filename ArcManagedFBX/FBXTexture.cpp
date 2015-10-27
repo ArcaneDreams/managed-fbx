73e2154c92779ea8a772dcd27f0199c5f2e58aae
Luc Shelton <lucshelton@gmail.com>
2015-10-27 12:20:16 +0000

    Committing in the implementation of FBXTexture

---
+++ ArcManagedFBX/FBXTexture.cpp
@@ -0,0 +1,10 @@
+#include "Stdafx.h"
+#include "FBXObject.h"
+#include "FBXTexture.h"
+
+using namespace ArcManagedFBX;
+using namespace ArcManagedFBX::Utility;
+
+ARC_DEFAULT_CONSTRUCTORS_IMPL(FBXTexture)
+
+ARC_DEFAULT_INTERNAL_CONSTRUCTOR_INHERIT_IMPL(FBXTexture,FBXObject,FbxTexture)
